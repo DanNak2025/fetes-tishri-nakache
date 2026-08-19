b97932c6972a432e02cb2071386f365f9541ee43
Dan <dan@MacBookPro.lan>
2026-08-19 13:50:18 +0300

    Optimize planning for mobile use

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./dynamic.css";
+import "./mobile.css";
 
 export const metadata: Metadata = {
   title: "Planning des fêtes · 2026",
